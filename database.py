6a9e8d3cb93d75ca8981586f4d28a4ce260ca4d1
daniil777000-pixel <daniil777000@gmail.com>
2026-07-20 13:32:50 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models import Base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "sqlite:///kod168.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()  # <--- Base создаётся ЗДЕСЬ
 
 def get_db():
     return SessionLocal()
