1a0d98cb0a2dc2912024cf898ee793dfa8e9b963
daniil777000-pixel <daniil777000@gmail.com>
2026-07-20 09:59:44 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,24 +1,36 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 from config import DATABASE_URL
 
 
 engine = create_engine(
     DATABASE_URL,
-    echo=False
+    connect_args={
+        "check_same_thread": False
+    }
 )
 
+
 SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
     bind=engine
 )
+
 
 Base = declarative_base()
 
 
 def get_db():
+    """
+    Создание сессии базы данных
+    """
+
     db = SessionLocal()
+
     try:
         return db
+
     finally:
-        db.close()
+        pass
